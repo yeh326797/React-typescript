bcfda9d580fd9648dbd1e4e07db2428a4f7fd09d
Pearl Yeh <pearl.yeh@cloudlytics.me>
2021-10-31 22:12:17 +0800

    Chapter3 Event

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDom from "react-dom";
 // import GuestList from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+// import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
   return (
     <div>
-      <UserSearch />
+      <EventComponent />
     </div>
   );
 };
